8773c9ed8e4b704fecfad9d54839d37119b8b851
Xinsheng (Shawn) Qin <xinshengqin2014@gmail.com>
2017-08-15 18:44:27 -0700

    add a macro

--- Src/Base/AMReX_fort_mod.F90
+++ Src/Base/AMReX_fort_mod.F90
@@ -71,4 +71,64 @@ contains
 
   end subroutine get_loop_bounds
 
+#ifdef CUDA
+  attributes(device) &
+#endif
+  subroutine get_loop_bounds_2d(blo, bhi, lo, hi)
+
+    implicit none
+
+    integer, intent(in   ) :: lo(2), hi(2)
+    integer, intent(inout) :: blo(2), bhi(2)
+
+#ifdef CUDA
+    ! Get our spatial index based on the CUDA thread index
+
+    blo(1) = lo(1) + (threadIdx%x - 1) + blockDim%x * (blockIdx%x - 1)
+    blo(2) = lo(2) + (threadIdx%y - 1) + blockDim%y * (blockIdx%y - 1)
+
+    ! If we have more threads than zones, set hi < lo so that the
+    ! loop iteration gets skipped.
+
+    if (blo(1) .gt. hi(1) .or. blo(2) .gt. hi(2)) then
+       bhi = blo - 1
+    else
+       bhi = blo
+    endif
+#else
+    blo = lo
+    bhi = hi
+#endif
+
+  end subroutine get_loop_bounds_2d
+
+! #ifdef CUDA
+!   attributes(device) &
+! #endif
+!   subroutine cuda_thread_bounds_2d(lo, hi)
+! 
+!     implicit none
+! 
+!     integer, intent(inout) :: lo(2), hi(2)
+! 
+! #ifdef CUDA
+!     ! Get our spatial index based on the CUDA thread index
+! 
+!     lo(1) = lo(1) + (threadIdx%x - 1) + blockDim%x * (blockIdx%x - 1)
+!     lo(2) = lo(2) + (threadIdx%y - 1) + blockDim%y * (blockIdx%y - 1)
+! 
+!     ! If we have more threads than zones, set hi < lo so that the
+!     ! loop iteration gets skipped.
+! 
+!     if (lo(1) .gt. hi(1) .or. lo(2) .gt. hi(2)) then
+!        hi = lo - 1
+!     else
+!        hi = lo
+!     endif
+! #else
+!     ! keep lo and hi unchanged
+! #endif
+! 
+!   end subroutine cuda_thread_bounds_2d
+
 end module amrex_fort_module
