07c8957f79b33ddb67acf8fea19af8ddc62d4a60
Xinsheng (Shawn) Qin <xinshengqin2014@gmail.com>
2017-08-24 14:38:23 -0700

    merge some kernels in slope_3d.F90

--- Tutorials/GPU/Advection_AmrCore/Source/Src_3d/slope_3d.F90
+++ Tutorials/GPU/Advection_AmrCore/Source/Src_3d/slope_3d.F90
@@ -2,6 +2,8 @@ module slope_module
 #ifdef CUDA
   use cuda_module, only: threads_and_blocks, stream_from_index
   use cuda_module, only: numThreads, numBlocks, cuda_streams 
+#else
+  use mempool_module, only : bl_allocate, bl_deallocate
 #endif
  
   implicit none
@@ -29,40 +31,73 @@ contains
     double precision, intent(out) :: dq(dqlo(1):dqhi(1),dqlo(2):dqhi(2),dqlo(3):dqhi(3))
 
     integer :: i, j, k
-    double precision, dimension(lo(1)-1:hi(1)+1) :: dsgn, dlim, df, dcen
+    ! double precision, dimension(lo(1)-1:hi(1)+1,lo(2):hi(2),lo(3):hi(3)) :: dsgn, dlim, df, dcen
     double precision :: dlft, drgt, dq1
 
 #ifdef CUDA
-    attributes(device) :: q, dq, dsgn, dlim, df, dcen
+    attributes(device) :: q, dq
+    ! attributes(device) :: dsgn, dlim, df, dcen
     integer, intent(in) :: idx, device_id
 #endif
 
+#ifdef CUDA
+    double precision, device, allocatable :: dsgn(:,:,:), dlim(:,:,:), df(:,:,:), dcen(:,:,:)
+#else
+    ! Some compiler may not support 'contiguous'.  Remove it in that case.
+    double precision, dimension(:,:,:), pointer, contiguous :: dsgn, dlim, df, dcen
+#endif
+
+#ifdef CUDA
+    allocate(dsgn  (lo(1)-1: hi(1)+1, lo(2): hi(2), lo(3): hi(3)) )
+    allocate(dlim  (lo(1)-1: hi(1)+1, lo(2): hi(2), lo(3): hi(3)) )
+    allocate(df    (lo(1)-1: hi(1)+1, lo(2): hi(2), lo(3): hi(3)) )
+    allocate(dcen  (lo(1)-1: hi(1)+1, lo(2): hi(2), lo(3): hi(3)) )
+#else
+    call bl_allocate(dsgn, lo(1)-1, hi(1)+1, lo(2), hi(2), lo(3), hi(3))
+    call bl_allocate(dlim, lo(1)-1, hi(1)+1, lo(2), hi(2), lo(3), hi(3))
+    call bl_allocate(df  , lo(1)-1, hi(1)+1, lo(2), hi(2), lo(3), hi(3))
+    call bl_allocate(dcen, lo(1)-1, hi(1)+1, lo(2), hi(2), lo(3), hi(3))
+#endif
+
+    !$cuf kernel do(3) <<<*, (8,8,4), 0, cuda_streams(stream_from_index(idx),device_id)>>> 
     do    k = lo(3), hi(3)
        do j = lo(2), hi(2)
-
-          ! first compute Fromm slopes
-          !$cuf kernel do <<<*, 64, 0, cuda_streams(stream_from_index(idx),device_id)>>> 
           do i = lo(1)-1, hi(1)+1
              dlft = q(i  ,j,k) - q(i-1,j,k)
              drgt = q(i+1,j,k) - q(i  ,j,k)
-             dcen(i) = .5d0 * (dlft+drgt)
-             dsgn(i) = sign(1.d0, dcen(i))
+             dcen(i,j,k) = .5d0 * (dlft+drgt)
+             dsgn(i,j,k) = sign(1.d0, dcen(i,j,k))
              if (dlft*drgt .ge. 0.d0) then
-                dlim(i) = 2.d0 * min( abs(dlft), abs(drgt) )
+                dlim(i,j,k) = 2.d0 * min( abs(dlft), abs(drgt) )
              else
-                dlim(i) = 0.d0
+                dlim(i,j,k) = 0.d0
              endif
-             df(i) = dsgn(i)*min( dlim(i), abs(dcen(i)) )
-          end do
-          
-          ! Now limited fourth order slopes
-          !$cuf kernel do <<<*, 64, 0, cuda_streams(stream_from_index(idx),device_id)>>> 
-          do i = lo(1), hi(1)
-             dq1 = four3rd*dcen(i) - sixth*(df(i+1) + df(i-1))
-             dq(i,j,k) = dsgn(i)*min(dlim(i),abs(dq1))
+             df(i,j,k) = dsgn(i,j,k)*min( dlim(i,j,k), abs(dcen(i,j,k)) )
           end do
        end do
     end do
+          
+    !$cuf kernel do(3) <<<*, (8,8,4), 0, cuda_streams(stream_from_index(idx),device_id)>>> 
+    do    k = lo(3), hi(3)
+       do j = lo(2), hi(2)
+          do i = lo(1), hi(1)
+             dq1 = four3rd*dcen(i,j,k) - sixth*(df(i+1,j,k) + df(i-1,j,k))
+             dq(i,j,k) = dsgn(i,j,k)*min(dlim(i,j,k),abs(dq1))
+          end do
+       end do
+    end do
+
+#ifdef CUDA
+    deallocate(dsgn)
+    deallocate(dlim)
+    deallocate(df)
+    deallocate(dcen)
+#else
+    call bl_deallocate(dsgn)
+    call bl_deallocate(dlim)
+    call bl_deallocate(df)
+    call bl_deallocate(dcen)
+#endif
 
   end subroutine slopex
 
@@ -89,28 +124,28 @@ contains
 #endif
 
 #ifdef CUDA
-    double precision, device, allocatable :: dsgn(:,:), dlim(:,:), df(:,:), dcen(:,:)
+    double precision, device, allocatable :: dsgn(:,:,:), dlim(:,:,:), df(:,:,:), dcen(:,:,:)
 #else
     ! Some compiler may not support 'contiguous'.  Remove it in that case.
-    double precision, dimension(:,:), pointer, contiguous :: dsgn, dlim, df, dcen
+    double precision, dimension(:,:,:), pointer, contiguous :: dsgn, dlim, df, dcen
 #endif
 
 #ifdef CUDA
-    allocate(dsgn  (lo(1): hi(1), lo(2)-1: hi(2)+1) )
-    allocate(dlim  (lo(1): hi(1), lo(2)-1: hi(2)+1) )
-    allocate(df    (lo(1): hi(1), lo(2)-1: hi(2)+1) )
-    allocate(dcen  (lo(1): hi(1), lo(2)-1: hi(2)+1) )
+    allocate(dsgn  (lo(1): hi(1), lo(2)-1: hi(2)+1, lo(3):hi(3)) )
+    allocate(dlim  (lo(1): hi(1), lo(2)-1: hi(2)+1, lo(3):hi(3)) )
+    allocate(df    (lo(1): hi(1), lo(2)-1: hi(2)+1, lo(3):hi(3)) )
+    allocate(dcen  (lo(1): hi(1), lo(2)-1: hi(2)+1, lo(3):hi(3)) )
 #else
-    call bl_allocate(dsgn, lo(1), hi(1), lo(2)-1, hi(2)+1)
-    call bl_allocate(dlim, lo(1), hi(1), lo(2)-1, hi(2)+1)
-    call bl_allocate(df  , lo(1), hi(1), lo(2)-1, hi(2)+1)
-    call bl_allocate(dcen, lo(1), hi(1), lo(2)-1, hi(2)+1)
+    call bl_allocate(dsgn, lo(1), hi(1), lo(2)-1, hi(2)+1, lo(3), hi(3))
+    call bl_allocate(dlim, lo(1), hi(1), lo(2)-1, hi(2)+1, lo(3), hi(3))
+    call bl_allocate(df  , lo(1), hi(1), lo(2)-1, hi(2)+1, lo(3), hi(3))
+    call bl_allocate(dcen, lo(1), hi(1), lo(2)-1, hi(2)+1, lo(3), hi(3))
 #endif
 
     call slopey_doit(lo, hi, &
                      q, qlo, qhi, &
                      dq, dqlo, dqhi, &
-                     dsgn, dlim, df, dcen, (/lo(1),lo(2)-1/), (/hi(1),hi(2)+1/) &
+                     dsgn, dlim, df, dcen, (/lo(1),lo(2)-1,lo(3)/), (/hi(1),hi(2)+1,hi(3)/) &
 #ifdef CUDA
                     , idx, device_id &
 #endif
@@ -140,13 +175,13 @@ contains
                          )
 
     integer, intent(in) :: lo(3), hi(3), qlo(3), qhi(3), dqlo(3), dqhi(3), &
-         ddlo(2), ddhi(2)
+         ddlo(3), ddhi(3)
     double precision, intent(in ) ::  q  ( qlo(1): qhi(1), qlo(2): qhi(2), qlo(3): qhi(3))
     double precision, intent(out) :: dq  (dqlo(1):dqhi(1),dqlo(2):dqhi(2),dqlo(3):dqhi(3))
-    double precision              :: dsgn(ddlo(1):ddhi(1),ddlo(2):ddhi(2))
-    double precision              :: dlim(ddlo(1):ddhi(1),ddlo(2):ddhi(2))
-    double precision              :: df  (ddlo(1):ddhi(1),ddlo(2):ddhi(2))
-    double precision              :: dcen(ddlo(1):ddhi(1),ddlo(2):ddhi(2))
+    double precision              :: dsgn(ddlo(1):ddhi(1),ddlo(2):ddhi(2),ddlo(3):ddhi(3))
+    double precision              :: dlim(ddlo(1):ddhi(1),ddlo(2):ddhi(2),ddlo(3):ddhi(3))
+    double precision              :: df  (ddlo(1):ddhi(1),ddlo(2):ddhi(2),ddlo(3):ddhi(3))
+    double precision              :: dcen(ddlo(1):ddhi(1),ddlo(2):ddhi(2),ddlo(3):ddhi(3))
 
     integer :: i, j, k
     double precision :: dlft, drgt, dq1
@@ -155,33 +190,32 @@ contains
     attributes(device) :: q, dq, dsgn, dlim, df, dcen
 #endif
 
+    !$cuf kernel do(3) <<<*, (8,8,4), 0, cuda_streams(stream_from_index(idx),device_id)>>> 
     do k = lo(3), hi(3)
-       ! first compute Fromm slopes
-       !$cuf kernel do(2) <<<(*,*), (16,16), 0, cuda_streams(stream_from_index(idx),device_id)>>> 
        do j    = lo(2)-1, hi(2)+1
           do i = lo(1)  , hi(1)
              dlft = q(i,j  ,k) - q(i,j-1,k)
              drgt = q(i,j+1,k) - q(i,j  ,k)
-             dcen(i,j) = .5d0 * (dlft+drgt)
-             dsgn(i,j) = sign( 1.d0, dcen(i,j) )
+             dcen(i,j,k) = .5d0 * (dlft+drgt)
+             dsgn(i,j,k) = sign( 1.d0, dcen(i,j,k) )
              if (dlft*drgt .ge. 0.d0) then
-                dlim(i,j) = 2.d0 * min( abs(dlft), abs(drgt) )
+                dlim(i,j,k) = 2.d0 * min( abs(dlft), abs(drgt) )
              else
-                dlim(i,j) = 0.d0
+                dlim(i,j,k) = 0.d0
              endif
-             df(i,j) = dsgn(i,j)*min( dlim(i,j),abs(dcen(i,j)) )
+             df(i,j,k) = dsgn(i,j,k)*min( dlim(i,j,k),abs(dcen(i,j,k)) )
           end do
        end do
+    end do
        
-       ! Now compute limited fourth order slopes
-       !$cuf kernel do(2) <<<(*,*), (16,16), 0, cuda_streams(stream_from_index(idx),device_id)>>> 
+    !$cuf kernel do(3) <<<*, (8,8,4), 0, cuda_streams(stream_from_index(idx),device_id)>>> 
+    do k = lo(3), hi(3)
        do j    = lo(2), hi(2)
           do i = lo(1), hi(1)
-             dq1 = four3rd*dcen(i,j) - sixth*( df(i,j+1) + df(i,j-1) )
-             dq(i,j,k) = dsgn(i,j)*min(dlim(i,j),abs(dq1))
+             dq1 = four3rd*dcen(i,j,k) - sixth*( df(i,j+1,k) + df(i,j-1,k) )
+             dq(i,j,k) = dsgn(i,j,k)*min(dlim(i,j,k),abs(dq1))
           end do
        end do
-
     end do
 
   end subroutine slopey_doit
